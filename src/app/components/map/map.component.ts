52dbd77fc390bb849a13f8b9e486d66fe7d9bd8d
ivan <ibodnar@outlook.com>
2018-12-07 00:50:29 -0300

    MapComponent -> se inyecta MapService y se usa mapService.lineasArray como capa geoJson, que se agrega al mapa

--- src/app/components/map/map.component.ts
+++ src/app/components/map/map.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+
+import {MapService} from '../../services/map.service';
+import {LineaModel} from '../../models/linea.model';
 // import {L} from 'leaflet';
 
 declare const L;
@@ -10,14 +13,23 @@ declare const L;
 })
 export class MapComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private mapService: MapService
+  ) { }
 
   ngOnInit() {
+
     const map = L.map('map').setView([-34.608891, -58.448817], 12);
 
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map);
+
+    setTimeout(() => {
+      L.geoJSON(this.mapService.lineasArray).addTo(map);
+    });
+
   }
+
 
 }
