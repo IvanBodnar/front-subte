b5f29883c68cc01cf82b20769c82ef5b6da23a89
ivan <ibodnar@outlook.com>
2018-12-07 00:51:00 -0300

    Se importa HttpClientModule en app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
+import {HttpClientModule} from '@angular/common/http';
+
 import { AppComponent } from './app.component';
 import { MapComponent } from './components/map/map.component';
 
@@ -12,7 +13,8 @@ import { MapComponent } from './components/map/map.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
