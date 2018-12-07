b7fa0483c6313147e8bf19ee70c8c1a0860a24dc
ivan <ibodnar@outlook.com>
2018-12-07 00:47:28 -0300

    MapService -> inyectado DataService e implementado init method que se llama desde app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
 
+import {MapService} from './services/map.service';
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'front-subte';
+  title = 'Subtes';
+
+  constructor(
+    private mapService: MapService
+  ) {
+    this.mapService.init();
+  }
+
 }
--- src/app/services/map.service.ts
+++ src/app/services/map.service.ts
@@ -1,9 +1,24 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {DataService} from './data.service';
+import {LineaModel} from '../models/linea.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MapService {
+  lineasArray: LineaModel[];
 
-  constructor() { }
+  constructor(
+    private dataService: DataService
+  ) { }
+
+  init(): void {
+    this.dataService.getLineas()
+      .subscribe(
+        lineas => {
+          this.lineasArray = lineas;
+        }
+      );
+  }
+
 }
