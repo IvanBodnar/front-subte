c30e598087892d786410654f782a36946af612f8
ivan <ibodnar@outlook.com>
2018-12-06 22:38:24 -0300

    Inicialización de map en map ts

--- src/app/components/map/map.component.ts
+++ src/app/components/map/map.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+// import {L} from 'leaflet';
+
+declare const L;
 
 @Component({
   selector: 'app-map',
@@ -10,6 +13,11 @@ export class MapComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    const map = L.map('map').setView([-34.608891, -58.448817], 12);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
   }
 
 }
